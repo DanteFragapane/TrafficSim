625229cfca34748d84a028bf42f67ab7052c3e93
Dante Fragapane <dante.fragapane@gmail.com>
2018-06-03 22:07:00 -0400

    Modified source.cpp

--- TrafficSim2/Source.cpp
+++ TrafficSim2/Source.cpp
@@ -1,45 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include "Home.h"
-//#include "male.txt"
-//#include "female.txt"
 using namespace std;
-
-string male_names[] = { "Guillermo", "Chase","Israel", "Reynaldo", "Burton", "Dante", "Max", "Luca", "Troy", "Luis", "Matt", "Nate" };
-string female_names[] = { "Aleisha", "Roseline", "Maple", "Cori", "Delia", "Ginger", "Katheryn", "Selene", "Kari", "Marlo", "Kayce", "Faustina" };
-string family_names[] = { "Kileen", "Cormack", "Doody", "Machuca", "Bartell", "Mac", "Maroney", "Shawl", "Mercurio", "Rochin", "Casanova", "Shine" };
-int length_of_name_lists = 12;
 
 int main()
 {
-	/*
-	Person Dante = Person("Dante");
-	Person Max = Person("Max");
-	Person Luca = Person("Luca");
-	vector<Person> names = { Dante, Max, Luca };
-	Family Fragapanes = Family("Fragapane", names);
-
-	Person Troy = Person("Troy");
-	names = { Troy };
-	Family Sextons = Family("Sexton", names);
-
-	vector<Family> families = { Fragapanes, Sextons };
-	Home Home1 = Home("Home1", families);
-	*/
-
-
 	vector<Person> maleList;
 	vector<Person> femaleList;
+	vector<string> familyNames;
 	vector<Family> families;
 	vector<Home> homes;
-	
-	for (int i = 0; i < length_of_name_lists; i++) {
-		maleList.push_back(Person(male_names[i]));
-		femaleList.push_back(Person(female_names[i]));
+
+	ifstream mFile;
+	ifstream fFile;
+	mFile.open("Male_Names.txt");
+	fFile.open("Female_Names.txt");
+	string maleName;
+	string femaleName;
+
+	if (!mFile || !fFile) {
+		cout << "Unable to open file";
 	}
-	for (int j = 0; j < length_of_name_lists; j++) {
-		vector<Person> names = { male_names[j], female_names[j] };
-		families.push_back(Family(family_names[j], names));
+	if (mFile && fFile) {
+		for (int i = 0; i < 100; i++) {
+			mFile >> maleName;
+			fFile >> femaleName;
+			femaleList.push_back(Person(femaleName));
+
+			if (i % 2 == 0) { // If even, i.e. first names only!
+				maleList.push_back(Person(maleName));
+				cout << "FIRST: " + maleName;
+				cout << endl;
+				cout << "FIRST: " + femaleName;
+			}
+			else { // Odds, i.e. last names!
+				familyNames.push_back(maleName);
+				cout << "LAST: " + maleName;
+			}
+			cout << endl;
+			cout << endl;
+		}
 	}
 
+	for (int j = 0; j < 50; j++) {
+		vector<Person> names = { maleList.at(j), femaleList.at(j) };
+		families.push_back(Family(familyNames[j], names));
+	}
+	
 }
