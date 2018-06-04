fce62f0c42056bf4593212a7a5c78c597af91290
Dante Fragapane <dante.fragapane@gmail.com>
2018-06-03 21:19:59 -0400

    Updated source.cpp

--- TrafficSim2/Source.cpp
+++ TrafficSim2/Source.cpp
@@ -6,10 +6,13 @@
 using namespace std;
 
 string male_names[] = { "Guillermo", "Chase","Israel", "Reynaldo", "Burton", "Dante", "Max", "Luca", "Troy", "Luis", "Matt", "Nate" };
-
+string female_names[] = { "Aleisha", "Roseline", "Maple", "Cori", "Delia", "Ginger", "Katheryn", "Selene", "Kari", "Marlo", "Kayce", "Faustina" };
+string family_names[] = { "Kileen", "Cormack", "Doody", "Machuca", "Bartell", "Mac", "Maroney", "Shawl", "Mercurio", "Rochin", "Casanova", "Shine" };
+int length_of_name_lists = 12;
 
 int main()
 {
+	/*
 	Person Dante = Person("Dante");
 	Person Max = Person("Max");
 	Person Luca = Person("Luca");
@@ -22,10 +25,21 @@ int main()
 
 	vector<Family> families = { Fragapanes, Sextons };
 	Home Home1 = Home("Home1", families);
+	*/
 
-	vector<Person> personList;
-	for (int i = 0; i < 12; i++) {
-		personList.push_back(Person(male_names[i]));
+
+	vector<Person> maleList;
+	vector<Person> femaleList;
+	vector<Family> families;
+	vector<Home> homes;
+	
+	for (int i = 0; i < length_of_name_lists; i++) {
+		maleList.push_back(Person(male_names[i]));
+		femaleList.push_back(Person(female_names[i]));
+	}
+	for (int j = 0; j < length_of_name_lists; j++) {
+		vector<Person> names = { male_names[j], female_names[j] };
+		families.push_back(Family(family_names[j], names));
 	}
 
 }
